331e082a34ff1b6d88ea28fa86e23dcab1b5d8fc
Muthuraj C <cmuthuraj544@gmail.com>
2024-07-29 16:08:38 +0530

    Create majority_element.cpp

---
+++ Arrays/Two_Pointer_Technique/majority_element.cpp
@@ -0,0 +1,40 @@
+//Problem link - https://leetcode.com/problems/majority-element/
+//Article link - https://www.geeksforgeeks.org/majority-element/
+
+//Solved using Boyer-Moore voting algorithm
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = -1, votes = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            if(votes == 0)
+            {
+                candidate = nums[i];
+            }
+            else
+            {
+                if(candidate == nums[i])
+                    votes++;
+                else
+                    votes--;
+            }            
+        }
+
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            cout << nums[i] << " " << candidate << endl;
+            if(nums[i] == candidate)
+                count++;
+        }
+        
+        if(count > n / 2)
+            return candidate;
+        
+        return -1;
+    }
+};
