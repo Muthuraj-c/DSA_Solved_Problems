07ed6f748f76e6436d4491cf74c26c252604a24b
Muthuraj-c <170298647+Muthuraj-c@users.noreply.github.com>
2024-08-06 13:17:40 +0530

    Created search_a_2D_matrix.cpp

---
+++ Matrix/search_a_2D_matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int start = 0, end = m * n - 1;
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int elem = matrix[row][col];
+
+            if (elem == target)
+                return true;
+            else if (elem < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+
+        return false;
+    }
+};
